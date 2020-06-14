b42e2740dd64efef8892f6f65b9854389d915524
Brian McPherson <heat3032@gmail.com>
2020-06-14 00:31:43 -0400

    v_2. Still needs form and social media links configured

---
+++ app.js
@@ -0,0 +1,10 @@
+M.AutoInit();
+
+// Slider
+const slider = document.querySelector(".slider");
+M.Slider.init(slider, {
+  indicators: false,
+  height: 500,
+  transition: 500,
+  interval: 6000,
+});
